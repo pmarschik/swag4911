265e0b760f61c4a2edc6e1078dc309dccb3650b3
Benjamin Bachhuber <benbachhuber@gmail.com>
2011-04-27 12:35:16 +0200

    added some unit tests for dao

---
+++ swag49/swag49.model/src/test/java/swag49/dao/MapDaoTest.java
@@ -0,0 +1,49 @@
+package swag49.dao;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import swag49.model.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/test-context.xml"})
+public class MapDaoTest {
+	// must use interface, qualifier is optional, use only if several beans that match interface
+    @Autowired @Qualifier("mapDAO")
+    private DataAccessObject<Map> mapDAO;
+
+    @Test
+    public void create_shouldCreate() throws Exception {
+		Map map = new Map();
+		map.setMaxUsers(5);
+		
+		map = mapDAO.create(map);
+    }
+    
+    @Test
+    public void delete_shouldDelete() throws Exception {
+		Map map = new Map();
+		map.setMaxUsers(5);
+		
+		map = mapDAO.create(map);
+		
+		mapDAO.delete(map);
+    }
+    
+    @Test
+    public void update_shouldUpdate() throws Exception{
+		Map map = new Map();
+		map.setMaxUsers(5);
+		
+		map = mapDAO.create(map);
+		
+		map.setMaxUsers(1);
+		
+		mapDAO.update(map);
+    }
+}
