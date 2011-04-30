ac31c89e1e4dfb60abb1d9e7d22f8b26b43fad97
Benjamin Bachhuber <benbachhuber@gmail.com>
2011-04-30 18:52:31 +0200

    added building dao

---
+++ swag49/swag49.model/src/main/java/swag49/dao/BuildingDao.java
@@ -0,0 +1,47 @@
+package swag49.dao;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import swag49.model.Building;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository(value="buildingDAO")
+public class BuildingDao implements DataAccessObject<Building> {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public BuildingDao() {
+	}
+
+	public Building get(Long id) {
+		return em.find(Building.class, id);
+	}
+
+	@Transactional
+	public Building create(Building building) {
+		return em.merge(building);
+	}
+
+	@Transactional
+	public Building update(Building building) {
+		return em.merge(building);
+	}
+
+	@Transactional
+	public void delete(Building building) {
+		building = em.merge(building);
+		em.remove(building);
+	}
+
+	public boolean contains(Building building)
+	{
+		return em.contains(building);
+	}
+}
+package src.main.java;
+
+public class BuildingDao {
+
+}
