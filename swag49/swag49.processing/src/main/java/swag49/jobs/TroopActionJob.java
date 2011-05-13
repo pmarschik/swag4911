5d6d8b258d24e0a2ad148b6081895b5db360744d
Matthias Rauch <rauchmat@gmail.com>
2011-05-13 09:44:06 +0200

    implemented TroopActionListener

--- swag49/swag49.processing/src/main/java/swag49/jobs/TroopActionJob.java
+++ swag49/swag49.processing/src/main/java/swag49/jobs/TroopActionJob.java
@@ -3,12 +3,22 @@ package swag49.jobs;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import swag49.dao.DataAccessObject;
+import swag49.model.TroopAction;
 
 public class TroopActionJob implements Job {
 
+	@Autowired @Qualifier("troopActionDAO")
+	private DataAccessObject<TroopAction> troopActionDao;
+	
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
-		
+		Long actionId = context.getJobDetail().getJobDataMap().getLong("actionId");
+		TroopAction action = troopActionDao.get(actionId);
+		//TODO to work with action.
 	}
 	
 }
--- swag49/swag49.processing/src/main/java/swag49/listener/TroopActionListener.java
+++ swag49/swag49.processing/src/main/java/swag49/listener/TroopActionListener.java
@@ -3,15 +3,21 @@ package swag49.listener;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
-import javax.persistence.PostPersist;
+import java.util.ArrayList;
 
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
+
+import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import swag49.jobs.TroopActionJob;
+import swag49.model.Troop;
 import swag49.model.TroopAction;
 
 public class TroopActionListener {
@@ -19,10 +25,17 @@ public class TroopActionListener {
 	@Autowired
 	private Scheduler scheduler;
 
+	@PostRemove
+	public void postRemove(TroopAction action) throws SchedulerException {
+		scheduler.deleteJob(getJobKey(action));
+	}
+
 	@PostPersist
 	public void postPersist(TroopAction action) throws SchedulerException {
+
 		JobDetail jobDetail = newJob(TroopActionJob.class).withIdentity(
-				action.getId().toString(), action.getClass().getName()).build();
+				getJobKey(action)).usingJobData("actionId", action.getId())
+				.build();
 
 		Trigger trigger = newTrigger().withIdentity(action.getId().toString(),
 				action.getClass().getName()).startAt(action.getEndDate())
@@ -30,4 +43,21 @@ public class TroopActionListener {
 
 		scheduler.scheduleJob(jobDetail, trigger);
 	}
+
+	private JobKey getJobKey(TroopAction action) {
+		return new JobKey(action.getId().toString(), action.getClass()
+				.getName());
+	}
+
+//	private JobDataMap getJobDataMap(TroopAction action) {
+//		JobDataMap jobData = new JobDataMap();
+//		ArrayList<Long> troopIds = new ArrayList<Long>();
+//		for (Troop troop : action.getConcerns()) {
+//			troopIds.add(troop.getId());
+//		}
+//		jobData.put("actionId", action.getId());
+//		jobData.put("tile", action.getTarget().getId());
+//		jobData.put("player", action.getPlayer().getId());
+//		return jobData;
+//	}
 }
