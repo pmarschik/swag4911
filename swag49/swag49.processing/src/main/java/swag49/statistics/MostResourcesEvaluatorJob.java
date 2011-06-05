5c14a345e79cfe6a7bc977df7e4fcb8875b760ca
Matthias Rauch <rauchmat@gmail.com>
2011-06-06 00:08:40 +0200

    MostResourcesEvaluatorJob finished

---
+++ swag49/swag49.processing/src/main/java/swag49/statistics/MostResourcesEvaluatorJob.java
@@ -0,0 +1,23 @@
+package swag49.statistics;
+
+import swag49.model.Player;
+
+public class MostResourcesEvaluatorJob extends StatisticJobBase {
+
+    public static final String NAME = "Most resources";
+
+    @Override
+    protected String getRankedPlayersQuery() {
+        return "select player from Player player order by (player.resources.amount_crops + player.resources.amount_gold + player.resources.amount_stone + player.resources.amount_wood) desc, player.id asc";
+    }
+
+    @Override
+    protected int getScore(Player player) {
+        return player.getResources().getAmount_crops() + player.getResources().getAmount_gold() + player.getResources().getAmount_stone() + player.getResources().getAmount_wood();
+    }
+
+    @Override
+    protected String getStatisticName() {
+        return NAME;
+    }
+}
